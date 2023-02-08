0b069b98de93ae87986a0ed972feee68c70aebe5
alvanfath <fathuralvan28@gmail.com>
2023-02-08 09:19:55 +0700

    git pull origin Master

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Admin\LoginAdminController;
 
@@ -22,6 +23,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('register', [RegisterController::class, 'register'])->name('register');
 Route::get('verif-email/{token}', [RegisterController::class,'verifEmail'])->name('verif-email');
-Route::prefix('_webmin')->name('login')->group(function () {
+Route::prefix('/webmin')->name('webmin.')->group(function () {
     Route::post('login', [LoginAdminController::class, 'login'])->name('login');
+
+    //category
+    Route::prefix('/category')->name('category.')->group(function () {
+        Route::post('/store', [CategoryController::class,'store'])->name('store');
+        Route::get('/edit/{id}', [CategoryController::class,'edit'])->name('edit');
+        Route::put('/update/{id}', [CategoryController::class,'update'])->name('update');
+        Route::delete('/destroy/{id}', [CategoryController::class, 'destroy'])->name('destroy');
+    }); 
 });
